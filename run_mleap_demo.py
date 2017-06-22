8ddb8cbf519399186ed7333fa1835d9783a64761
Jason Slepicka <jasonslepicka@gmail.com>
2017-06-21 21:51:13 -0700

    Even more spark magic config

--- run_mleap_demo.py
+++ run_mleap_demo.py
@@ -2,6 +2,6 @@ from subprocess import call
 import os
 cwd = os.getcwd()
 call(["python", "configure_sparkmagic.py"])
-call(["sudo", "-u", "jupyter", "jupyter", "nbconvert",  "--config", cwd + "/.jupyter/jupyter_notebook_config.py", "--ExecutePreprocessor.timeout=600", "--to", "notebook", "--output", "/tmp/spark", "--execute", 'Spark ML Pipelines and MLeap Demo.ipynb'])
+call(["jupyter", "nbconvert",  "--config", cwd + "/.jupyter/jupyter_notebook_config.py", "--ExecutePreprocessor.timeout=600", "--to", "notebook", "--output", "/tmp/spark", "--execute", 'Spark ML Pipelines and MLeap Demo.ipynb'])
 call(["cat", "/tmp/spark.ipynb"])
 call(["jq", ".cells[].outputs[].text?", "/tmp/spark.ipynb"])
