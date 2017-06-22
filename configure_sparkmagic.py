9f176be1390b18659e12e33eef27f8341c799b22
Jason Slepicka <jasonslepicka@gmail.com>
2017-06-21 21:23:48 -0700

    Update run scripts to enable spark magic

--- configure_sparkmagic.py
+++ configure_sparkmagic.py
@@ -1,2 +1,81 @@
 from subprocess import call
-call(["cat", "/home/jupyter/.sparkmagic/config.json"])
+import os
+
+sparkmagic_config = """"
+{
+  "kernel_python_credentials" : {
+    "username": "",
+    "password": "",
+    "url": "http://10.30.1.32:8998"
+  },
+
+  "kernel_scala_credentials" : {
+    "username": "",
+    "password": "",
+    "url": "http://10.30.1.32:8998"
+  },
+
+  "logging_config": {
+    "version": 1,
+    "formatters": {
+      "magicsFormatter": {
+        "format": "%(asctime)s\t%(levelname)s\t%(message)s",
+        "datefmt": ""
+      }
+    },
+    "handlers": {
+      "magicsHandler": {
+        "class": "hdijupyterutils.filehandler.MagicsFileHandler",
+        "formatter": "magicsFormatter",
+        "home_path": "~/.sparkmagic"
+      }
+    },
+    "loggers": {
+      "magicsLogger": {
+        "handlers": ["magicsHandler"],
+        "level": "DEBUG",
+        "propagate": 0
+      }
+    }
+  },
+
+  "wait_for_idle_timeout_seconds": 15,
+  "status_sleep_seconds": 2,
+  "statement_sleep_seconds": 2,
+  "livy_session_startup_timeout_seconds": 60,
+
+  "fatal_error_suggestion": "The code failed because of a fatal error:\n\t{}.\n\nSome things to try:\na) Make sure Spark has enough
+available resources for Jupyter to create a Spark context.\nb) Contact your Jupyter administrator to make sure the Spark magics libr
+ary is configured correctly.\nc) Restart the kernel.",
+
+  "ignore_ssl_errors": false,
+
+  "session_configs": {
+    "driverMemory": "1000M",
+    "executorCores": 2,
+    "conf":{"spark.jars.packages":"org.apache.hadoop:hadoop-aws:2.7.3,org.apache.hadoop:hadoop-azure:2.7.3"}
+  },
+
+  "use_auto_viz": true,
+  "max_results_sql": 2500,
+  "pyspark_dataframe_encoding": "utf-8",
+
+  "heartbeat_refresh_seconds": 30,
+  "livy_server_heartbeat_timeout_seconds": 0,
+  "heartbeat_retry_seconds": 10,
+
+  "server_extension_default_kernel_name": "pysparkkernel"
+}
+"""
+os.mkdir("/home/jupyter/.sparkmagic")
+sparkmagic_config_file = open("/home/jupyter/.sparkmagic/config.json", "w")
+sparkmagic_config_file.write(sparkmagic_config)
+sparkmagic_config_file.close()
+cwd = os.getcwd()
+call(["jupyter", "notebook", "--generate-config"])
+call(["pip", "install", "sparkmagic"])
+#call(["pip3", "install", "sparkmagic"])
+call(["jupyter", "nbextension", "enable", "--py", "--sys-prefix", "widgetsnbextension"])
+call(["jupyter", "serverextension", "enable", "--py", "sparkmagic"])
+#call(["jupyter-kernelspec", "install", "/usr/local/lib/python3.5/dist-packages/sparkmagic/kernels/sparkkernel"])
+call(["jupyter-kernelspec", "install", "/usr/local/lib/python2.7/dist-packages/sparkmagic/kernels/sparkkernel"])
--- run_mleap_demo.py
+++ run_mleap_demo.py
@@ -1,3 +1,6 @@
 from subprocess import call
-call(["jupyter", "nbconvert",  "--ExecutePreprocessor.timeout=600", "--to", "notebook", "--output", "spark", "--execute", 'Spark ML Pipelines and MLeap Demo.ipynb'])
+import os
+cwd = os.getcwd()
+call(["python", "configure_sparkmagic.py"])
+call(["jupyter", "nbconvert",  "--config", cwd + "/.jupyter/jupyter_notebook_config.py", "--ExecutePreprocessor.timeout=600", "--to", "notebook", "--output", "spark", "--execute", 'Spark ML Pipelines and MLeap Demo.ipynb'])
 call(["jq", ".cells[].outputs[].text?", "spark.ipynb"])
